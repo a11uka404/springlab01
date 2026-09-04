d7618026cae740cfe68a4fa2d110db672ca739b3
A11uka <aabdrakhim@icloud.com>
2026-09-04 21:54:56 +0600

    lab01: generated Spring Boot project skeleton

---
+++ src/test/java/kz/iitu/spring/lab01/SpringLab01ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.spring.lab01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLab01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
